f6161006d48d77c1b087b1712ee609a93c424d81
Christian Bremer <bremerseegooglemail.com>
2019-12-22 23:05:35 +0100

    Version 1.3.0-SNAPSHOT with JDK 11

--- docker/entry.sh
+++ docker/entry.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-CONFIG_PASSWORD="$(cat /run/secrets/config-server-client-user-password)"
+export CONFIG_PASSWORD="$(cat /run/secrets/config-server-client-user-password)"
 java -Djava.security.egd=file:/dev/./urandom -jar /opt/app.jar
