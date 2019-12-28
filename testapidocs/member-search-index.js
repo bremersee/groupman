0b50d4257670712555cbeb26eed5b5470ffe9a18
Jenkins <jenkins@jenkins.bremersee.lokal>
2019-12-28 02:05:55 +0100

    Site checkin for project groupman

---
+++ testapidocs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"org.bremersee.groupman","c":"ApplicationTests","l":"ApplicationTests()","url":"%3Cinit%3E()"},{"p":"org.bremersee.groupman","c":"ApplicationTests","l":"contextLoads()"},{"p":"org.bremersee.groupman","c":"ApplicationTests","l":"setup()"}]
