0b39e9ab25c9f832e7bdfdddf2debfc2b087d64e
Christian Bremer <bremerseegooglemail.com>
2019-12-28 01:41:43 +0100

    Version 1.3.0-SNAPSHOT with JDK 8

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 if [ -z "$CONFIG_PASSWORD" ] && [ ! -z "$CONFIG_PASSWORD_FILE" ] && [ -e $CONFIG_PASSWORD_FILE ]; then
   export CONFIG_PASSWORD="$(cat $CONFIG_PASSWORD_FILE)"
 fi
