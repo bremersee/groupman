2a5f7d9e9b7bca92cc10015f2e8bd39e0cae5405
Christian Bremer <bremersee@googlemail.com>
2024-12-15 16:37:13 +0100

    Version 3.0.0-SNAPSHOT

--- src/main/java/org/bremersee/groupman/config/WebSecurityConfiguration.java
+++ src/main/java/org/bremersee/groupman/config/WebSecurityConfiguration.java
@@ -19,12 +19,14 @@ package org.bremersee.groupman.config;
 import static java.util.Objects.requireNonNull;
 import static org.springframework.util.ObjectUtils.isEmpty;
 
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.bremersee.spring.security.ldaptive.authentication.ReactiveLdaptiveAuthenticationManager;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.boot.actuate.health.HealthEndpoint;
 import org.springframework.boot.actuate.info.InfoEndpoint;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -38,6 +40,10 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
 import org.springframework.security.config.web.server.ServerHttpSecurity.FormLoginSpec;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -53,6 +59,7 @@ import reactor.core.publisher.Mono;
  */
 @EnableWebFluxSecurity
 @EnableConfigurationProperties({
+    SecurityProperties.class,
     OAuth2ResourceServerProperties.class
 })
 @Configuration
@@ -68,21 +75,55 @@ public class WebSecurityConfiguration {
   /**
    * Instantiates a new web security configuration.
    *
+   * @param securityProperties the security properties
    * @param resourceServerProperties the resource server properties
    * @param jwtConverterProvider the jwt converter provider
-   * @param ldaptiveAuthenticationManager the ldaptive authentication manager
-   * @param inMemory the in memory
+   * @param authenticationManagers the authentication managers
    */
   public WebSecurityConfiguration(
+      SecurityProperties securityProperties,
       OAuth2ResourceServerProperties resourceServerProperties,
       ObjectProvider<Converter<Jwt, AbstractAuthenticationToken>> jwtConverterProvider,
-      ObjectProvider<ReactiveLdaptiveAuthenticationManager> ldaptiveAuthenticationManager,
-      ObjectProvider<UserDetailsRepositoryReactiveAuthenticationManager> inMemory) {
+      List<ReactiveAuthenticationManager> authenticationManagers) {
     this.resourceServerProperties = resourceServerProperties;
     this.jwtAuthenticationConverter = jwtConverterProvider
         .getIfAvailable(JwtAuthenticationConverter::new);
-    ReactiveAuthenticationManager tmp = ldaptiveAuthenticationManager.getIfAvailable();
-    this.basicAuthenticationManager = tmp != null ? tmp : inMemory.getIfAvailable();
+    this.basicAuthenticationManager = authenticationManagers.stream()
+        .filter(authenticationManager -> authenticationManager
+            instanceof ReactiveLdaptiveAuthenticationManager)
+        .findFirst()
+        .map(authenticationManager -> {
+          log.info("Using ldaptive authentication manager for basic authentication.");
+          return authenticationManager;
+        })
+        .orElseGet(() -> {
+          if (!authenticationManagers.isEmpty()) {
+            log.info("Using authentication managers for basic authentication: {}",
+                authenticationManagers.get(0));
+            return authenticationManagers.get(0);
+          } else {
+            return createInMemoryAuthenticationManager(securityProperties);
+          }
+        });
+  }
+
+  private static ReactiveAuthenticationManager createInMemoryAuthenticationManager(
+      SecurityProperties securityProperties) {
+    log.info("Using in-memory authentication manager with username '{}' and password '{}'.",
+        securityProperties.getUser().getName(), securityProperties.getUser().getPassword());
+    String password = securityProperties.getUser().getPassword();
+    if (!password.startsWith("{noop}")) {
+      password = "{noop}" + password;
+    }
+    UserDetails userDetails = User.builder()
+        .username(securityProperties.getUser().getName())
+        .password(password)
+        .roles(securityProperties.getUser().getRoles().toArray(new String[0]))
+        .build();
+    ReactiveUserDetailsService userDetailsService = new MapReactiveUserDetailsService(
+        List.of(userDetails));
+    return new UserDetailsRepositoryReactiveAuthenticationManager(userDetailsService);
+
   }
 
   /**
@@ -125,17 +166,9 @@ public class WebSecurityConfiguration {
         .headers(configurer -> configurer
             .frameOptions(c -> c.mode(Mode.SAMEORIGIN)))
 
+        .httpBasic(customizer -> customizer
+            .authenticationManager(basicAuthenticationManager))
         .formLogin(FormLoginSpec::disable);
-
-    if (!isEmpty(basicAuthenticationManager)) {
-      log.info("Basic authentication manager enabled: {}", basicAuthenticationManager);
-      http
-          .httpBasic(customizer -> customizer
-              .authenticationManager(basicAuthenticationManager));
-
-    } else {
-      log.info("Basic authentication manager disabled.");
-    }
 
     if (!isEmpty(resourceServerProperties.getJwt().getJwkSetUri())) {
       log.info("Authentication of bearer token enabled: {}",
